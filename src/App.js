923640109daef147a6b89f81f2a7ab5011c16b13
MarkoStrahilov <72885606+MarkoStrahilov@users.noreply.github.com>
2022-12-12 21:58:38 +0100

    react-router update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import './App.css';
 import Dashboard from "./DashboardTemplate/Dashboard";
 
@@ -6,7 +7,28 @@ function App() {
     <div className="App">
       <Dashboard/>
     </div>
+=======
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import MenuPage from './Menu/MenuPage';
+import HomePage from './Home/HomePage';
+import AboutPage from './Contact/AboutPage';
+import ContactPage from './Contact/ContactPage';
+
+
+function App() {
+  return (
+    <Router className="App">
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/menu' element={<MenuPage />}/>
+        <Route path='/about' element={<AboutPage />}/>
+        <Route path='/contact' element={<ContactPage />}/>
+      </Routes>
+    </Router>
+>>>>>>> Stashed changes
   );
 }
 
 export default App;
+
