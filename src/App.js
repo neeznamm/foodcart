81f577865843645cb16e551afd1236c1fc46cb56
MarkoStrahilov <72885606+MarkoStrahilov@users.noreply.github.com>
2022-12-13 13:21:53 +0100

    dashboard update

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import MenuPage from './Menu/MenuPage';
 import Dashboard from './DashboardTemplate/Dashboard';
 import AboutPage from './Contact/AboutPage';
 import ContactPage from './Contact/ContactPage.jsx';
-import Orders from './Menu/Orders';
+import Orders from './Orders/Orders';
 import ErrorPage from './ErrorPage';
 
 function App() {
   return (
-    <Router className="App">
-      <Routes>
-        <Route path='/' element={<Dashboard />}/>
-        <Route path='/menu' element={<MenuPage />}/>
-        <Route path='/about' element={<AboutPage />}/>
-        <Route path='/contact' element={<ContactPage />}/>
-        <Route path='/orders' element={<Orders />}/>
-        <Route path='/*' element={<ErrorPage />}/>
-      </Routes>
-    </Router>
+    <div className="App">
+      <Router>
+        <Dashboard />
+        <Routes>
+          <Route path='/menu' element={<MenuPage />}/>
+          <Route path='/about' element={<AboutPage />}/>
+          <Route path='/contact' element={<ContactPage />}/>
+          <Route path='/orders' element={<Orders />}/>
+          <Route path='/*' element={<ErrorPage />}/>
+        </Routes>
+      </Router>
+      </div>
   );
 }
 
