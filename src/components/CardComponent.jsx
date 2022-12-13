4c64052b7a05bbe88d1458c636c52f13edfa04f4
AnaAtanasova <92518840+AnaAtanasova@users.noreply.github.com>
2022-12-13 15:46:57 +0100

    Modified CardComponent

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,19 +1,11 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
+import { Card, CardActions, CardContent, CardHeader, Grid } from '@mui/material';
 import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
+import pizzaPhoto from '../images/pizza_peppers.png'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Button, Typography, IconButton, Collapse, Rating, } from '@mui/material';
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -36,39 +28,22 @@ export default function RecipeReviewCard() {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        title="Paneera - Pizza Place"
+        subheader="Pepperoni Pizza"
       />
-      <CardMedia
-        component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
-      />
+      <img src={pizzaPhoto} alt="pizzaPhoto" width={350} height={400} />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
+          Integriants: Tomato Sauce, Mozzarella, Loads od pepperoni
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
+        <Grid xs={4}>
+          <IconButton aria-label="add to chart">
+            <FavoriteIcon />
+          </IconButton>
+        </Grid>
+        <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
         <ExpandMore
           expand={expanded}
           onClick={handleExpandClick}
