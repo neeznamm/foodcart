0bad8616cac01cf80d6d6daa835d69235bf4e773
AnaAtanasova <92518840+AnaAtanasova@users.noreply.github.com>
2022-12-13 21:51:40 +0100

    Add data to component

--- src/Menu/MenuPage.jsx
+++ src/Menu/MenuPage.jsx
@@ -1,17 +1,76 @@
-import React from 'react'
-import { Grid, Typography } from "@mui/material";
-import CardComponent from '../components/CardComponent'
-import Dashboard from 'src/DashboardTemplate/Dashboard';
-import Items from '../DashboardTemplate/listItems';
+import React, { useEffect, useState } from "react";
+import { Grid } from "@mui/material";
+import CardComponent from "../components/CardComponent";
+import { MEALS } from "src/data/meals";
 
 const MenuPage = () => {
-  return (
-    <>
-    <Grid>
-      <CardComponent/>
-    </Grid>
-  </>
-  )
-}
+  const [meals, setMeals] = useState([]);
+  const [mealsFiltered, setMealsFiltered] = useState([]);
+  const [mealsLoading, setMealsLoading] = useState(true);
 
-export default MenuPage
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const fetchMeals = async () => {
+    try {
+      // const response = await fetch("meals.json", {
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //     Accept: "application/json",
+      //   },
+      // });
+
+      // console.log(response)
+
+      // const mealsObject = await response.json();
+
+      const mealsObject = MEALS;
+
+      const meals = Object.values(mealsObject);
+
+      setMeals(meals);
+      setMealsLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const mealMatchesQuery = (meal, query) => {
+    return meal.name.toLowerCase().includes(query.toLowerCase());
+  };
+
+  useEffect(() => {
+    fetchMeals();
+  }, []);
+
+  useEffect(
+    () =>
+      setMealsFiltered(
+        meals.filter((meal) => mealMatchesQuery(meal, searchQuery))
+      ),
+    [meals, searchQuery]
+  );
+
+  return (
+    <Grid container spacing={2} sx={{p: 1}}>
+      {mealsLoading ? (
+        <div>Loading...</div>
+      ) : (
+        mealsFiltered.map(
+          ({ name, description, price, img, restaurantName }) => (
+            <Grid item>
+              <CardComponent
+                name={name}
+                description={description}
+                price={price}
+                img={img}
+                restaurantName={restaurantName}
+              />
+            </Grid>
+          )
+        )
+      )}
+    </Grid>
+  );
+};
+
+export default MenuPage;
--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,24 +1,42 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import { Card, CardActions, CardContent, CardHeader, Grid } from '@mui/material';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import pizzaPhoto from '../images/pizza_peppers.png'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { Button, Typography, IconButton, Collapse, Rating, } from '@mui/material';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Grid,
+} from "@mui/material";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import pizzaPhoto from "../images/pizza_peppers.png";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import {
+  Button,
+  Typography,
+  IconButton,
+  Collapse,
+  Rating,
+} from "@mui/material";
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
 })(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
     duration: theme.transitions.duration.shortest,
   }),
 }));
 
-export default function RecipeReviewCard() {
+const RecipeReviewCard = ({
+  name,
+  description,
+  price,
+  img,
+  restaurantName,
+}) => {
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
@@ -27,19 +45,17 @@ export default function RecipeReviewCard() {
 
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        title="Paneera - Pizza Place"
-        subheader="Pepperoni Pizza"
-      />
-      <img src={pizzaPhoto} alt="pizzaPhoto" width={350} height={400} />
+      <CardHeader title={restaurantName} subheader={name} />
+      <img src={img} alt="" width={350} height={400} />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          Integriants: Tomato Sauce, Mozzarella, Loads od pepperoni
+          {description}
         </Typography>
+        <Typography>{price}</Typography>
       </CardContent>
       <CardActions disableSpacing>
         <Grid xs={4}>
-          <IconButton aria-label="add to chart">
+          <IconButton aria-label="add to cart">
             <FavoriteIcon />
           </IconButton>
         </Grid>
@@ -57,31 +73,39 @@ export default function RecipeReviewCard() {
         <CardContent>
           <Typography paragraph>Method:</Typography>
           <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
+            set aside for 10 minutes.
           </Typography>
           <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
+            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
+            stirring occasionally until lightly browned, 6 to 8 minutes.
+            Transfer shrimp to a large plate and set aside, leaving chicken and
+            chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
+            onion, salt and pepper, and cook, stirring often until thickened and
+            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
+            cups chicken broth; bring to a boil.
           </Typography>
           <Typography paragraph>
             Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don&apos;t open.)
+            peppers, and cook without stirring, until most of the liquid is
+            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
+            shrimp and mussels, tucking them down into the rice, and cook again
+            without stirring, until mussels have opened and rice is just tender,
+            5 to 7 minutes more. (Discard any mussels that don&apos;t open.)
           </Typography>
           <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
+            Set aside off of the heat to let rest for 10 minutes, and then
+            serve.
           </Typography>
         </CardContent>
       </Collapse>
     </Card>
   );
-}
+};
+
+export default RecipeReviewCard;
+
+RecipeReviewCard.defaultProps = {
+  restaurantName: "Restaurant Name",
+};
