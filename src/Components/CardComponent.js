2649fb9251706b8051bb15b2a52be0f139584b58
AnaAtanasova <92518840+AnaAtanasova@users.noreply.github.com>
2022-12-14 23:15:00 +0100

    Changed buttons

--- src/Components/CardComponent.js
+++ src/Components/CardComponent.js
@@ -9,23 +9,8 @@ import {
 } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import {
-  Typography,
-  IconButton,
-  Collapse,
-  Rating,
-} from "@mui/material";
-
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton />;
-})(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+import { Typography, IconButton, Collapse, Rating } from "@mui/material";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 
 const RecipeReviewCard = ({
   name,
@@ -52,51 +37,17 @@ const RecipeReviewCard = ({
       </CardContent>
       <CardActions disableSpacing>
         <Grid item xs={4}>
-          <IconButton aria-label="add to cart">
-            <FavoriteIcon />
+          <IconButton
+            aria-label="add to cart"
+            onClick={() => {
+              alert("Added to cart");
+            }}
+          >
+            <ShoppingBasketIcon />
           </IconButton>
         </Grid>
         <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
       </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-            set aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-            stirring occasionally until lightly browned, 6 to 8 minutes.
-            Transfer shrimp to a large plate and set aside, leaving chicken and
-            chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-            onion, salt and pepper, and cook, stirring often until thickened and
-            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-            cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is
-            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
-            shrimp and mussels, tucking them down into the rice, and cook again
-            without stirring, until mussels have opened and rice is just tender,
-            5 to 7 minutes more. (Discard any mussels that don&apos;t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then
-            serve.
-          </Typography>
-        </CardContent>
-      </Collapse>
     </Card>
   );
 };
--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,18 +1,12 @@
 import { useLocation } from "react-router-dom";
 import React, { useEffect, useState, useNavigate } from "react";
-import { Grid } from "@mui/material";
+import { Grid, Pagination } from "@mui/material";
 import CardComponent from "../Components/CardComponent";
 import { MEALS } from "src/data/meals";
 import axios from "axios";
 
 const Menu = () => {
   //const location = useLocation();
-  // const [meals, setMeals] = useState([]);
-  // const [mealsFiltered, setMealsFiltered] = useState([]);
-  // const [mealsLoading, setMealsLoading] = useState(true);
-
-  //const [searchQuery, setSearchQuery] = useState("");
-
   const [products, setProducts] = useState([]);
   const [productsFiltered, setProductsFiltered] = useState([]);
   const [productsLoading, setProductsLoading] = useState(false);
@@ -21,46 +15,6 @@ const Menu = () => {
   const [pagesFetched, setPagesFetched] = useState([]);
   const [pageIdxsFetched, setPageIdxsFetched] = useState([]);
   //const navigate = useNavigate();
-
-  // const fetchMeals = async () => {
-  //   try {
-  //     // const response = await fetch("meals.json", {
-  //     //   headers: {
-  //     //     "Content-Type": "application/json",
-  //     //     Accept: "application/json",
-  //     //   },
-  //     // });
-
-  //     // console.log(response)
-
-  //     // const mealsObject = await response.json();
-
-  //     const mealsObject = MEALS;
-
-  //     const meals = Object.values(mealsObject);
-
-  //     setMeals(meals);
-  //     setMealsLoading(false);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-  // const mealMatchesQuery = (meal, query) => {
-  //   return meal.name.toLowerCase().includes(query.toLowerCase());
-  // };
-
-  // useEffect(() => {
-  //   fetchMeals();
-  // }, []);
-
-  // useEffect(
-  //   () =>
-  //     setMealsFiltered(
-  //       meals.filter((meal) => mealMatchesQuery(meal, searchQuery))
-  //     ),
-  //   [meals, searchQuery]
-  // );
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -84,26 +38,31 @@ const Menu = () => {
   }, [currentPage]);
 
   return (
-    <Grid container spacing={3} >
+    <>
+    <Grid container spacing={3}>
       {productsLoading ? (
         <div>Loading...</div>
       ) : (
-        products.map(
-          ({ name, description, price, category, index }) => (
-            <Grid item key={index}>
-              <CardComponent
-                name={name}
-                description={description}
-                price={price}
-                //img={img}
-                //restaurantName={restaurantName}
-                category={category}
-              />
-            </Grid>
-          )
-        )
+        products.map(({ name, description, price, category, index }) => (
+          <Grid item key={index}>
+            <CardComponent
+              name={name}
+              description={description}
+              price={price}
+              //img={img}
+              //restaurantName={restaurantName}
+              category={category}
+            />
+          </Grid>
+        ))
       )}
     </Grid>
+    <Pagination
+    count={10}
+    page={currentPage}
+    onChange={(event, page) => setCurrentPage(page)}
+  />
+  </>
   );
 };
 
