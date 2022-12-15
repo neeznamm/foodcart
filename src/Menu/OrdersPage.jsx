6818442b9c3b4dafe972dd1beab6a2db55e0dba7
MarkoStrahilov <72885606+MarkoStrahilov@users.noreply.github.com>
2022-12-15 15:19:38 +0100

    Update OrdersPage.jsx

--- src/Menu/OrdersPage.jsx
+++ src/Menu/OrdersPage.jsx
@@ -2,19 +2,11 @@ import React, { useState, useContext} from 'react'
 import OrderForm from '../components/OrderForm'
 import Orderlist from 'src/components/OrderList'
 import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Alert from '@mui/material/Alert';
-import IconButton from '@mui/material/IconButton';
-import Collapse from '@mui/material/Collapse';
-import CloseIcon from '@mui/icons-material/Close';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 import EmptyCard from 'src/components/Dashboard/EmptyCard';
 import CartContext from 'src/context/CartContext';
 
