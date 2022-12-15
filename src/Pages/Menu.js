db57e2a295ab7d84e084929bb8ef8785700b0a69
unknown <mlviktor23@gmail.com>
2022-12-15 19:55:24 +0100

    styled offers page (onblur, clickable menu button...)

--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -4,6 +4,7 @@ import {useLocation} from "react-router-dom";
 
 const Menu = () => {
     const location = useLocation()
+    console.log(location.state);
     return(
         <>
         <p><b>Primer info za restoran</b><br/>id:{location.state.id}<br/>{location.state.name} <br/>tags: {location.state.tags} <br/>itn itn.</p>
--- src/Pages/Offers.js
+++ src/Pages/Offers.js
@@ -5,15 +5,56 @@ import {Button, Chip, CircularProgress, Grid, Pagination, Rating} from "@mui/mat
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
-import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 import {useNavigate} from "react-router-dom";
 import {customTheme} from "../shared/Theme";
 import {ThemeProvider} from "@mui/material/styles";
-import MenuBookIcon from '@mui/icons-material/MenuBook';
 import {ArrowBack, ArrowForward} from "@mui/icons-material";
-import CardMedia from "@mui/material/CardMedia";
 import foodImages from '../shared/ImageGetter.js'
 import Box from "@mui/material/Box";
+
+const OfferCard = (props) => {
+  const {r} = props;
+  const navigate = useNavigate();
+  const [hovered, setHovered] = useState(false);
+
+  return <Grid item xs={2} sm={3} md={4} key={r.index}>
+    <div className="positioner" style={{position:"relative"}}>
+    <Card onMouseEnter={()=>setHovered(true)} onMouseLeave={()=>setHovered(false)}
+          variant="outlined" sx={{
+            filter: hovered ? "blur(8px)" : null,
+            boxShadow:'5px 5px 30px #8888', backgroundImage:`linear-gradient(rgba(255,255,255,0.7), rgba(255,255,255,0.7)), 
+          url(${foodImages[(r.tags.length+r.name.length)%17]})`,
+      maxHeight:"260px", height:"100%",':hover': {boxShadow: 20}}}>
+      <CardContent>
+        {/*{renderDollarIcon (r.price_range.length)}*/}
+        <Box component="div" sx={{ p: 1, backgroundColor:"rgba(255,255,255,0.85)", height:"fit-content", width:"fit-content", mb:2, borderRadius:"8px" }}>
+          <Typography variant="h5" component="div">
+            {r.name}
+          </Typography>
+        </Box>
+        <div style={{height:"130px", float:"left"}}>
+          {r.tags.split(',').slice(0,6).map(t => <Chip component="span" key={t} label={t} sx={{m: 0.3, opacity:"0.9", fontStyle:"italic"}} color={t.length % 2 === 0 ? "primary" :
+              t.length % 13 === 0 ? "warning" : "success"}/>)}
+        </div>
+      </CardContent>
+      <CardActions>
+      </CardActions>
+      <CardActions sx={{position:"absolute", bottom:"10px", right:"10px", opacity:"0.9"}} >
+        <Button variant="contained"><Rating size="small" name="read-only" value={r.score} readOnly /></Button>
+      </CardActions>
+    </Card>
+      <Button sx={{position: "absolute", top:"50%", left:"50%", transform:"translate(-50%,-50%)", zIndex:hovered ? "100" : "-100"}}
+              variant="contained"
+              onClick={() => {
+                navigate('/menu', {state: r})
+              }
+              }
+              onMouseEnter={()=>setHovered(true)} onMouseLeave={()=>setHovered(false)}
+      >VIEW MENU
+      </Button>
+    </div>
+  </Grid>
+}
 
 const Offers = () => {
   const [restaurants, setRestaurants] = useState([]);
@@ -21,7 +62,7 @@ const Offers = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [pagesFetched, setPagesFetched] = useState([]);
   const [pageIdxsFetched, setPageIdxsFetched] = useState([]);
-  const navigate = useNavigate();
+  const [showOrderButton, setShowOrderButton] = useState(false);
 
   useEffect(() => {
     const fetchRestaurants = async () => {
@@ -32,61 +73,24 @@ const Offers = () => {
         pagesFetched.push(resp.data)
         pageIdxsFetched.push(currentPage);
       } else setRestaurants(pagesFetched[currentPage])
-
     }
+
     fetchRestaurants();
   },[currentPage])
-
-  const renderDollarIcon = (count) => {
-    const dollarSignsArray = []
-    for (let i=0;i<count;++i)
-      dollarSignsArray.push(<MonetizationOnIcon key={i} sx={{opacity:"0.7", color:'darkgreen'}}/>)
-    return dollarSignsArray;
-  }
 
   return restaurants ?
       (<ThemeProvider theme={customTheme}>
         <div style={{margin:"15px 60px 60px 60px"}}>
-        <Typography variant="h4" sx={{mb:3}}>
+        <Typography variant="h4" sx={{mt:1.5, mb:2, float:"left"}}>
           Choose a <b>restaurant</b> to order from</Typography>
+          <Pagination color="primary" count={10} page={currentPage} onChange={(event,page)=>setCurrentPage(page)}
+                      size="large" sx={{mt:1.5, ml:5, float:"right"}} slots={{ previous: <ArrowBack/>, next: <ArrowForward/> }}/>
 
     <Grid container spacing={{ xs: 2, md: 5 }} columns={{ xs: 4, sm: 8, md: 12 }}>
     {restaurants.map((r) => {
       const tags = r.tags.split(",");
-      return(
-          <Grid item xs={2} sm={3} md={3} key={r.index}>
-          <Card variant="outlined" sx={{boxShadow:'5px 5px 15px #8888', backgroundImage:`linear-gradient(rgba(255,255,255,0.7), rgba(255,255,255,0.7)), 
-          url(${foodImages[(r.tags.length+r.name.length)%17]})`,
-            maxHeight:"260px", height:"100%", position:"relative",':hover': {
-              boxShadow: 20, // theme.shadows[20]
-            }}}>
-        <CardContent>
-          {/*{renderDollarIcon (r.price_range.length)}*/}
-          <Box component="div" sx={{ p: 1, backgroundColor:"rgba(255,255,255,0.85)", height:"fit-content", width:"fit-content", mb:2, borderRadius:"8px" }}>
-          <Typography variant="h5" component="div">
-            {r.name}
-          </Typography>
-          </Box>
-          <div style={{height:"130px", float:"left"}}>
-            {tags.slice(0,6).map(t => <Chip component="span" key={t} label={t} sx={{m: 0.3, opacity:"0.9", fontStyle:"italic"}} color={t.length % 2 === 0 ? "primary" :
-                t.length % 13 == 0 ? "warning" : "success"}/>)}
-          </div>
-        </CardContent>
-        {/*<CardActions>*/}
-        {/*  <Button sx={{position:"absolute", bottom:"20px", right:"20px"}} variant="contained" endIcon={<MenuBookIcon />} onClick={() => {*/}
-        {/*    navigate('/menu',{state:r})*/}
-        {/*  }*/}
-        {/*  }*/}
-        {/*  >VIEW MENU</Button>*/}
-        {/*</CardActions>*/}
-        <CardActions sx={{position:"absolute", bottom:"10px", right:"10px", opacity:"0.9"}} >
-          <Button variant="contained" ><Rating name="read-only" value={r.score} readOnly /></Button>
-        </CardActions>
-      </Card>
-          </Grid>)
+      return <OfferCard key={r.index} r={r} />
     })}
-      <Pagination color="primary" count={10} page={currentPage} onChange={(event,page)=>setCurrentPage(page)}
-                  sx={{mt:18, ml:5}} slots={{ previous: <ArrowBack/>, next: <ArrowForward/> }}/>
         </Grid>
         </div>
       </ThemeProvider>)
